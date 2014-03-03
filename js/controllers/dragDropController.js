01fe177e20b77136808228aac5d737401a79b16d
johnedante <daniel.alvenstrand@gmail.com>
2014-03-03 15:41:26 +0100

    Simple test
    Simple test with drag and drop in HTML5, check it out and use it if
    you'd like.

--- js/controllers/dragDropController.js
+++ js/controllers/dragDropController.js
@@ -6,3 +6,17 @@ var DragDropController = function(view, model ) {
 	});
 
 }
+
+function allowDrop(ev){
+	ev.preventDefault();
+}
+
+function drag(ev){
+	ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev){
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("Text");
+	ev.target.appendChild(document.getElementById(data));
+}
