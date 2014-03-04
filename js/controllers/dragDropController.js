c626d1604c569cfe4c286129820b64c1df211f50
JamesNode <janis_rode@hotmail.com>
2014-03-04 11:31:57 +0100

    some more changes
    issues with addDay(); issues with creating a new activity for some
    reason and a possible issue with listeners not listening

--- js/controllers/dragDropController.js
+++ js/controllers/dragDropController.js
@@ -20,10 +20,9 @@ var index = 3;
 		$( ".droparea" ).sortable({
 			connectWith: ".droparea",
 			start: function(event, ui) { sta = $(ui.item[0]).position(); console.log(sta); },    
-			stop:function(event, ui) { sto = $(ui.item[0]).position(); console.log(sto); $(inner);}
+			stop: function(event, ui) { sto = $(ui.item[0]).position(); console.log(sto); $(inner);}
 		//could use naming system for starting location $(ui.item[0]).attr('id'); but not for stop
 		}).disableSelection();
-
 	}
 	function inner() {
 		var tt = 9;
@@ -36,6 +35,8 @@ var index = 3;
 		if (newday<0) {newday=null};
 		var oldposition = (sta.top-tt)/nt;
 		var newposition = (sto.top-tt)/nt;
+		console.log(oldday, oldposition, newday, newposition);
 		model.moveActivity(oldday, oldposition, newday, newposition);
+		
 		}
 
--- js/views/dragDropView.js
+++ js/views/dragDropView.js
@@ -5,32 +5,18 @@ var DragDropView = function (container,model) {
 	model.addObserver(this);
 	
 	
-	this.update = function(){
-	var parked = model.parkedActivities;
-	var first= "nullday";
-	for(var i=0;i<parked.length;i++){
-		var html= '<div id="'+first+i+'" class="'+parked[i].getType().replace(" ", "")+'"> '+parked[i].getLength()+' min '+parked[i].getName()+'</div>';
-		$("#div1").append(html);
-	}
-	var k = 0;
-		for(var j = 0;j<model.days.length;j++){
-		var day = model.days[j];
-		first = j+"day";
-	k=j+2;
-	for(var i=0;i<day._activities.length;i++){
-	var html= '<div id="'+first+i+'" class="'+day._activities[i].getType().replace(" ", "")+'"> '+day._activities[i].getLength()+' min '+day._activities[i].getName()+'</div>';
-	$("#div"+k).append(html);
-	}
-	}
+this.update = function(){
+$(as);
+}
+
+
 
 }
 $(as);
-
-}
-
 function as(){
 var parked = model.parkedActivities;
 	var first= "nullday";
+	$("#div1").html("");
 for(var i=0;i<parked.length;i++){
 	var html= '<div id="'+first+i+'" class="'+parked[i].getType().replace(" ", "")+'"> '+parked[i].getLength()+' min '+parked[i].getName()+'</div>';
 	$("#div1").append(html);
@@ -40,6 +26,7 @@ for(var i=0;i<parked.length;i++){
 		var day = model.days[j];
 		first = j+"day";
 	k=j+2;
+	$("#div"+k).html("");
 	for(var i=0;i<day._activities.length;i++){
 	var html= '<div id="'+first+i+'" class="'+day._activities[i].getType().replace(" ", "")+'"> '+day._activities[i].getLength()+' min '+day._activities[i].getName()+'</div>';
 	$("#div"+k).append(html);
